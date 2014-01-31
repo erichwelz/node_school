e24b7b10b1e3286df3ed2686cdbf440ef1493fa6
Erich Welz <erichwelz@gmail.com>
2014-01-31 11:41:54 -0500

    non-blocking ls -lah run on current directory from / or /start routes

--- server/server.js
+++ server/server.js
@@ -6,15 +6,11 @@ function start(route, handle) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname);
+    route(handle, pathname, response);
+    }
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }
-
-http.createServer(onRequest).listen(8888);
-console.log("Server has started.");
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
 
 exports.start = start;
