b8e5d2d59cce2d4b217d7ffc69306779090d4ef0
Erich Welz <erichwelz@gmail.com>
2014-02-02 15:37:02 -0500

    routes added showing test image

--- server/requestHandlers.js
+++ server/requestHandlers.js
@@ -1,7 +1,9 @@
 var querystring = require("querystring");
+  fs = require("fs");
 
 function start(response, postData) {
   console.log("Request handler 'start' was called.");
+
   var body = '<html>'+
   '<meta http-equiv="Content-Type" content="text/html"; '+
   'charset=UTF-8" />'+
@@ -27,5 +29,21 @@ function upload(response, postData) {
   response.end();
 }
 
+function show(response, postData) {
+  console.log("Request handler 'show' was called.");
+  fs.readFile("test.png", "binary", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end()
+    }
+  });
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
