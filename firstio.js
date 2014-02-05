c1566a21c9a934f7145e51da0f5e66b3fd66b061
Erich Welz <erichwelz@gmail.com>
2014-02-05 11:46:04 -0500

    manual file reorg

--- firstio.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs')
-var buffer = fs.readFileSync(process.argv[2])
-
-var arr = buffer.toString().split('\n')
-
-console.log(arr.length - 1)
--- ls.js
+++
@@ -1,1 +0,0 @@
-var dirModule = require ('./dirmodule.js')
